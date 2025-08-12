8944339f40835fe074976a3f0412797beee41ae5
Oliver Bryan <65399431+hex248@users.noreply.github.com>
2025-08-12 16:14:43 +0100

    more robust build+run script

--- go.sh
+++ go.sh
@@ -1,6 +1,27 @@
 #!/bin/bash
 
+# exit on any error
+set -e
+
+echo "current step: build"
+
 cd build
-cmake ..
-make
+
+echo "current step: cmake"
+if cmake ..; then
+    echo "CMAKE successful"
+else
+    echo "CMAKE failed"
+    exit 1
+fi
+
+echo "current step: make"
+if make; then
+    echo "BUILD successful"
+else
+    echo "BUILD failed"
+    exit 1
+fi
+
+echo "current step: run"
 ./factor-e
