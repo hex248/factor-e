2fd0067402df3428ec6a59b99077ad2293741dd0
Oliver Bryan <65399431+hex248@users.noreply.github.com>
2025-08-12 12:13:27 +0100

    font loading

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,9 @@
 #include "raylib.h"
+#include <iostream>
 
 int main() {
-    const int screenWidth = 800;
-    const int screenHeight = 450;
+    const int screenWidth = 1920;
+    const int screenHeight = 1080;
 
     InitWindow(screenWidth, screenHeight, "FACTOR E");
 
@@ -10,6 +11,11 @@ int main() {
 
     SetWindowState(FLAG_WINDOW_UNDECORATED);
     ToggleFullscreen();
+
+    Font fontLarge = LoadFontEx("assets/fonts/JetBrainsMono/JetBrainsMono-Regular.ttf", 128, 0, 250);
+    Font fontMedium = LoadFontEx("assets/fonts/JetBrainsMono/JetBrainsMono-Regular.ttf", 64, 0, 250);
+    Font fontSmall = LoadFontEx("assets/fonts/JetBrainsMono/JetBrainsMono-Regular.ttf", 32, 0, 250);
+    SetTextLineSpacing(16);
 
     while (!WindowShouldClose())
     {
@@ -33,10 +39,15 @@ int main() {
 
         ClearBackground(BLACK);
 
-        DrawFPS(10, 10);
+        // DrawFPS(10, 10);
+        DrawTextEx(fontSmall, "FACTOR E", (Vector2){ 0, 0 }, (float)fontSmall.baseSize, 2, WHITE);
 
         EndDrawing();
     }
+
+    UnloadFont(fontLarge);
+    UnloadFont(fontMedium);
+    UnloadFont(fontSmall);
 
     CloseWindow();
     return 0;
