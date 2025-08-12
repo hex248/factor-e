2ad5d520d899371aa01d9fb51d26ddf6b40913f7
Oliver Bryan <65399431+hex248@users.noreply.github.com>
2025-08-12 21:09:18 +0100

    removed "true monitor" debug

--- src/main.cpp
+++ src/main.cpp
@@ -159,16 +159,15 @@ int main() {
 
         
         if (DEBUG_MODE == 1) {
-            static char debugText[7][64];
+            static char debugText[6][64];
             snprintf(debugText[0], sizeof(debugText[0]), "Offset: %dx%d", (int)offsetX, (int)offsetY);
             snprintf(debugText[1], sizeof(debugText[1]), "Monitor Count: %d", GetMonitorCount());
             snprintf(debugText[2], sizeof(debugText[2]), "Current Monitor: %d", GetCurrentMonitor());
             snprintf(debugText[3], sizeof(debugText[3]), "Screen: %dx%d", screenWidth, screenHeight);
             snprintf(debugText[4], sizeof(debugText[4]), "Display Size: %dx%d", displayWidth, displayHeight);
-            snprintf(debugText[5], sizeof(debugText[5]), "True Monitor: %dx%d", trueMonitorWidth, trueMonitorHeight);
-            snprintf(debugText[6], sizeof(debugText[6]), "Screen Center: %.0fx%.0f", screenCenter.x, screenCenter.y);
+            snprintf(debugText[5], sizeof(debugText[5]), "Screen Center: %.0fx%.0f", screenCenter.x, screenCenter.y);
 
-            for (int i = 0; i < 7; i++) {
+            for (int i = 0; i < 6; i++) {
                 DrawTextEx(fontSmallExtraLight, debugText[i], (Vector2){ 0, i * 25 }, (float)fontSmallExtraLight.baseSize, 2, WHITE);
             }
         }
