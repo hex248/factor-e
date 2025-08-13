0fe3dc206d3757f1e07a94b17134f19cd56c6af3
Oliver Bryan <65399431+hex248@users.noreply.github.com>
2025-08-13 18:29:35 +0100

    formatting

--- src/main.cpp
+++ src/main.cpp
@@ -21,67 +21,74 @@ int displayWidth = DEFAULT_WINDOW_WIDTH;
 int displayHeight = DEFAULT_WINDOW_HEIGHT;
 int trueMonitorWidth = DEFAULT_WINDOW_WIDTH;
 int trueMonitorHeight = DEFAULT_WINDOW_HEIGHT;
-Vector2 screenCenter = { DEFAULT_WINDOW_WIDTH / 2.0f, DEFAULT_WINDOW_HEIGHT / 2.0f };
+Vector2 screenCenter = {DEFAULT_WINDOW_WIDTH / 2.0f, DEFAULT_WINDOW_HEIGHT / 2.0f};
 
-typedef struct Tile {
+typedef struct Tile
+{
     unsigned char id;
     char name[16];
     Color color;
 } Tile;
 
-typedef struct Map {
+typedef struct Map
+{
     unsigned int tilesX;
     unsigned int tilesY;
     unsigned char *tileIds;
     Tile *tiles;
 } Map;
 
-void UpdateScreenDimensions() {
+void UpdateScreenDimensions()
+{
     int monitor = GetCurrentMonitor();
 
-    if (IsWindowFullscreen()) {
+    if (IsWindowFullscreen())
+    {
         screenWidth = trueMonitorWidth;
         screenHeight = trueMonitorHeight;
         displayWidth = trueMonitorWidth;
         displayHeight = trueMonitorHeight;
-    } else {
+    }
+    else
+    {
         trueMonitorWidth = GetMonitorWidth(monitor);
         trueMonitorHeight = GetMonitorHeight(monitor);
         displayWidth = trueMonitorWidth;
         displayHeight = trueMonitorHeight;
-        
+
         screenWidth = GetRenderWidth();
         screenHeight = GetRenderHeight();
     }
-    
-    screenCenter = (Vector2){ screenWidth / 2.0f, screenHeight / 2.0f };
+
+    screenCenter = (Vector2){screenWidth / 2.0f, screenHeight / 2.0f};
 }
 
-void InitDisplaySystem() {
+void InitDisplaySystem()
+{
     InitWindow(DEFAULT_WINDOW_WIDTH, DEFAULT_WINDOW_HEIGHT, "FACTOR E");
 
     int monitor = GetCurrentMonitor();
 
     trueMonitorWidth = GetMonitorWidth(monitor);
     trueMonitorHeight = GetMonitorHeight(monitor);
-    
+
     displayWidth = trueMonitorWidth;
     displayHeight = trueMonitorHeight;
-    
+
     SetWindowState(FLAG_WINDOW_RESIZABLE);
     // ClearWindowState(FLAG_WINDOW_RESIZABLE);
-    
+
     SetWindowPosition(
         (trueMonitorWidth - DEFAULT_WINDOW_WIDTH) / 2,
-        (trueMonitorHeight - DEFAULT_WINDOW_HEIGHT) / 2
-    );
+        (trueMonitorHeight - DEFAULT_WINDOW_HEIGHT) / 2);
 
     UpdateScreenDimensions();
 }
 
-int main() {
+int main()
+{
     InitDisplaySystem();
-    
+
     SetTargetFPS(144);
 
     Font fontLarge = LoadFontEx("assets/fonts/JetBrainsMono/JetBrainsMono-Regular.ttf", 128, 0, 250);
@@ -93,36 +100,39 @@ int main() {
 
     SetTextLineSpacing(16);
 
-    Map map = { 0 };
+    Map map = {0};
     map.tilesX = MAP_SIZE_X;
     map.tilesY = MAP_SIZE_Y;
 
     float offsetX = (screenWidth - MAP_TILE_SIZE * MAP_SIZE_X) / 2;
     float offsetY = (screenHeight - MAP_TILE_SIZE * MAP_SIZE_Y) / 2;
 
-    map.tileIds = (unsigned char *)calloc(map.tilesX*map.tilesY, sizeof(unsigned char));
-    for (unsigned int i = 0; i < map.tilesY*map.tilesX; i++) map.tileIds[i] = i;
+    map.tileIds = (unsigned char *)calloc(map.tilesX * map.tilesY, sizeof(unsigned char));
+    for (unsigned int i = 0; i < map.tilesY * map.tilesX; i++)
+        map.tileIds[i] = i;
 
-    map.tiles = (Tile *)calloc(map.tilesX*map.tilesY, sizeof(Tile));
-    for (unsigned int i = 0; i < map.tilesY*map.tilesX; i++) {
+    map.tiles = (Tile *)calloc(map.tilesX * map.tilesY, sizeof(Tile));
+    for (unsigned int i = 0; i < map.tilesY * map.tilesX; i++)
+    {
         map.tiles[i].id = i;
         snprintf(map.tiles[i].name, sizeof(map.tiles[i].name), "%d", i + 1);
         int lightness = GetRandomValue(0, 120);
-        map.tiles[i].color = (Color){ lightness, lightness + 80, lightness, 255 };
+        map.tiles[i].color = (Color){lightness, lightness + 80, lightness, 255};
     }
 
     while (!WindowShouldClose())
     {
         UpdateScreenDimensions();
 
-        if (IsKeyPressed(KEY_F11)) ToggleBorderlessWindowed();
+        if (IsKeyPressed(KEY_F11))
+            ToggleBorderlessWindowed();
 
         offsetX = (screenWidth - MAP_TILE_SIZE * MAP_SIZE_X) / 2;
         offsetY = (screenHeight - MAP_TILE_SIZE * MAP_SIZE_Y) / 2;
 
         BeginDrawing();
 
-        ClearBackground((Color){ 10, 10, 10, 255 });
+        ClearBackground((Color){10, 10, 10, 255});
 
         for (unsigned int y = 0; y < map.tilesY; y++)
         {
@@ -130,37 +140,38 @@ int main() {
             {
                 int tileX = (int)(offsetX + x * MAP_TILE_SIZE);
                 int tileY = (int)(offsetY + y * MAP_TILE_SIZE);
-                
-                DrawRectangle(tileX, tileY, MAP_TILE_SIZE, MAP_TILE_SIZE, map.tiles[y*map.tilesX + x].color);
-                
-                Vector2 textSize = MeasureTextEx(fontSmall, map.tiles[y*map.tilesX + x].name, (float)fontSmall.baseSize, 2);
+
+                DrawRectangle(tileX, tileY, MAP_TILE_SIZE, MAP_TILE_SIZE, map.tiles[y * map.tilesX + x].color);
+
+                Vector2 textSize = MeasureTextEx(fontSmall, map.tiles[y * map.tilesX + x].name, (float)fontSmall.baseSize, 2);
                 Vector2 textPos = {
                     tileX + (MAP_TILE_SIZE - textSize.x) / 2,
-                    tileY + (MAP_TILE_SIZE - textSize.y) / 2
-                };
-                DrawTextEx(fontSmall, map.tiles[y*map.tilesX + x].name, textPos, (float)fontSmall.baseSize, 2, WHITE);
+                    tileY + (MAP_TILE_SIZE - textSize.y) / 2};
+                DrawTextEx(fontSmall, map.tiles[y * map.tilesX + x].name, textPos, (float)fontSmall.baseSize, 2, WHITE);
             }
         }
-        
+
         int gridStartX = (int)offsetX;
         int gridStartY = (int)offsetY;
         int gridEndX = gridStartX + MAP_SIZE_X * MAP_TILE_SIZE;
         int gridEndY = gridStartY + MAP_SIZE_Y * MAP_TILE_SIZE;
-        
-        for (unsigned int x = 0; x <= MAP_SIZE_X; x++) {
+
+        for (unsigned int x = 0; x <= MAP_SIZE_X; x++)
+        {
             int lineX = gridStartX + x * MAP_TILE_SIZE;
             DrawLine(lineX, gridStartY, lineX, gridEndY, WHITE);
         }
-        
-        for (unsigned int y = 0; y <= MAP_SIZE_Y; y++) {
+
+        for (unsigned int y = 0; y <= MAP_SIZE_Y; y++)
+        {
             int lineY = gridStartY + y * MAP_TILE_SIZE;
             DrawLine(gridStartX, lineY, gridEndX, lineY, WHITE);
         }
 
         DrawCircle((int)screenCenter.x, (int)screenCenter.y, 30.0f, WHITE);
 
-        
-        if (DEBUG_MODE) {
+        if (DEBUG_MODE)
+        {
             static char debugText[6][64];
             snprintf(debugText[0], sizeof(debugText[0]), "Offset: %dx%d", (int)offsetX, (int)offsetY);
             snprintf(debugText[1], sizeof(debugText[1]), "Monitor Count: %d", GetMonitorCount());
@@ -169,8 +180,9 @@ int main() {
             snprintf(debugText[4], sizeof(debugText[4]), "Display Size: %dx%d", displayWidth, displayHeight);
             snprintf(debugText[5], sizeof(debugText[5]), "Screen Center: %.0fx%.0f", screenCenter.x, screenCenter.y);
 
-            for (int i = 0; i < 6; i++) {
-                DrawTextEx(fontSmallExtraLight, debugText[i], (Vector2){ 0, i * 25 }, (float)fontSmallExtraLight.baseSize, 2, WHITE);
+            for (int i = 0; i < 6; i++)
+            {
+                DrawTextEx(fontSmallExtraLight, debugText[i], (Vector2){0, i * 25}, (float)fontSmallExtraLight.baseSize, 2, WHITE);
             }
         }
 
