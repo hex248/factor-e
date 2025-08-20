3e44e4e6dedcef56f3a19eef6edd3fe2bdc1616a
Oliver Bryan <65399431+hex248@users.noreply.github.com>
2025-08-20 17:10:46 +0100

    exit build directory after running

--- run.bat
+++ run.bat
@@ -4,10 +4,12 @@ if exist "build\Release\factor-e.exe" (
     echo running "Release" version...
     cd build\Release
     factor-e.exe
+    cd ../..
 ) else if exist "build\Debug\factor-e.exe" (
     echo running "Debug" version...
     cd build\Debug
     factor-e.exe
+    cd ../..
 ) else (
     echo error: factor-e.exe not found in "build\Release" or "build\Debug"
     echo make sure you have built the project first with: "build.bat"
