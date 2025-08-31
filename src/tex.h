12831bf004c25e1ea36d76c732c1df4e8beffb21
Oliver Bryan <65399431+hex248@users.noreply.github.com>
2025-08-31 18:08:20 +0100

    use const char * instead of char *

--- src/tex.h
+++ src/tex.h
@@ -1,13 +1,13 @@
 #include "raylib.h"
 
 unsigned char RegisterTexture(Image img);
-unsigned char RegisterTexture(char *file);
-char *RegisterTexture(Image img, char *customkey);
-char *RegisterTexture(char *file, char *customkey);
+unsigned char RegisterTexture(const char *file);
+char *RegisterTexture(Image img, const char *customkey);
+char *RegisterTexture(char *file, const char *customkey);
 
 Texture2D GetTexture(unsigned char key);
-Texture2D GetTexture(char *key);
+Texture2D GetTexture(const char *key);
 
 void UnloadTextures();
 void UnloadTexture(unsigned char key);
-void UnloadTexture(char *key);
+void UnloadTexture(const char *key);
